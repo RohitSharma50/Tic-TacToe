c3c642d42a28662a104777e818d6005ae8f63851
Rohit Sharma <rohitshar0801@gmail.com>
2022-04-23 12:15:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -24,28 +24,28 @@ if(winner) {
   updateGameState(initalState)
 }
 
+
 }, [gameState])
  
-const checkWinner =() =>{
-  const lines =[
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-
-  ]; 
-  for (let i=0; i<lines.length; i++) {
-    const [a, b, c] = lines[i]; 
-    if(gameState[a] && gameState[a]===gameState[b] && gameState[a] ===gameState[c]){
-   return gameState[a];
-  }  
-}
-  return null;
-}
+  function checkWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+        return gameState[a];
+      }
+    }
+    return null;
+  }
 
   return (
     <div className="app-header">
@@ -72,3 +72,4 @@ const checkWinner =() =>{
 }
 
 export default App;
+
